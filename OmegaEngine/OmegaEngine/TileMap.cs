be87334e4ac3af52303476aa405f871366cda35c
Sebastian <darklink.perry@gmail.com>
2019-06-03 11:57:55 -0400

    Added first classes, broken Map class into series of classes

---
+++ OmegaEngine/OmegaEngine/TileMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OmegaEngine
+{
+    class TileMap
+    {
+        public TileMap(Texture2D texture, Color? color = null)
+        {
+            Texture = texture;
+            Fill = color ?? Color.White;
+        }
+
+        public void Draw(Rectangle dimensions)
+        {
+            Globals.spriteBatch.Draw(Texture, dimensions, Fill);
+        }
+
+        private Texture2D Texture { get; set; }
+        private Color Fill { get; set; }
+    }
+}
